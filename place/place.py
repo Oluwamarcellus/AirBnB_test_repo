145399a507c7888fdb9943362ff34aa6ba273bec
Oluwamarcellus <noransaber685@gmail.com>
2023-08-09 18:19:11 +0300

    Updates

---
+++ place/place.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Place class"""
+from .base_model import BaseModel
+class Place(BaseModel):
+    """ Blueprint for place object"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
