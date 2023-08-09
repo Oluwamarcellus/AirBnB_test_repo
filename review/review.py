c897a68e305a23d3d24c88464f219dbf07887025
Oluwamarcellus <noransaber685@gmail.com>
2023-08-09 18:25:20 +0300

    Updates

--- review/review.py
+++ review/review.py
@@ -2,8 +2,9 @@
 """ Review Class"""
 from .base_model import BaseModel
 
+
 class Review(BaseModel):
     """Blueprint for review"""
     place_id = ""
     user_id = ""
-    text= ""
+    text = ""
